39ce768e9ae8adf2d2a45da857b9bb3754a4ff09
Lucas Sassi de Souza <sassi.lucas17@gmail.com>
2025-10-13 19:21:32 -0300

    Lista de alocacao dinamica terminada

--- alocacaoDinamica.c
+++ alocacaoDinamica.c
@@ -25,6 +25,65 @@ void exercicio2(){
     printf("\nTAMANHO DA STRUCT ALUNO: %d byte(s)\n", sizeof(Aluno));
 }
 
+struct Cadastro {
+    char nome[100];
+    int idade;
+    char endereco[200];
+};
+
+struct Cadastro* alocarVetor(int N) {
+    struct Cadastro* vetor = (struct Cadastro*) malloc(N * sizeof(struct Cadastro));
+    if (vetor == NULL) {
+        printf("Erro na alocacao de memoria!\n");
+        exit(1);
+    }
+    return vetor;
+}
+
+void preencherDados(struct Cadastro* vetor, int N) {
+    printf("\n--- Insercao de Dados ---\n");
+    for (int i = 0; i < N; i++) {
+        printf("\nCadastro %d:\n", i + 1);
+
+        printf("  Digite o nome: ");
+        while (getchar() != '\n');
+        fgets(vetor[i].nome, sizeof(vetor[i].nome), stdin);
+
+        printf("  Digite a idade: ");
+        scanf("%d", &vetor[i].idade);
+
+        printf("  Digite o endereco: ");
+        while (getchar() != '\n');
+        fgets(vetor[i].endereco, sizeof(vetor[i].endereco), stdin);
+    }
+}
+
+void exercicio3() {
+    int numero_de_cadastros;
+
+    printf("Digite o numero total de cadastros: ");
+    scanf("%d", &numero_de_cadastros);
+    
+    if (numero_de_cadastros <= 0) {
+        printf("Numero invalido.\n");
+        return;
+    }
+
+    struct Cadastro* meus_cadastros = alocarVetor(numero_de_cadastros);
+
+    preencherDados(meus_cadastros, numero_de_cadastros);
+
+    printf("\n--- Dados Cadastrados ---\n");
+    for (int i = 0; i < numero_de_cadastros; i++) {
+        printf("\nCadastro %d:\n", i + 1);
+        printf("  Nome: %s", meus_cadastros[i].nome);
+        printf("  Idade: %d\n", meus_cadastros[i].idade);
+        printf("  Endereco: %s", meus_cadastros[i].endereco);
+    }
+
+    free(meus_cadastros);
+}
+
 void exercicio4(){
     printf("\no tamanho do vetor a ser alocado dinamicamente: ");
     int n;
@@ -53,19 +112,74 @@ void exercicio4(){
 
 }
 
+int* criarEPreencherVetorValidado(int *tamanhoPtr) {
+    do {
+        printf("Digite um valor inteiro positivo para N: ");
+        scanf("%d", tamanhoPtr);
+
+        if (*tamanhoPtr <= 0) {
+            printf("Valor invalido! Por favor, digite um numero maior que zero.\n");
+        }
+    } while (*tamanhoPtr <= 0);
+
+    int *vetor = (int*) malloc(*tamanhoPtr * sizeof(int));
+    if (vetor == NULL) {
+        printf("Erro de alocacao de memoria!\n");
+        exit(1);
+    }
+
+    printf("\n--- Digite os %d elementos do vetor ---\n", *tamanhoPtr);
+    for (int i = 0; i < *tamanhoPtr; i++) {
+        do {
+            printf("Digite o valor para a posicao %d: ", i);
+            scanf("%d", &vetor[i]);
+
+            if (vetor[i] < 2) {
+                printf("Valor invalido! O numero deve ser maior ou igual a 2.\n");
+            }
+        } while (vetor[i] < 2);
+    }
+
+    return vetor;
+}
+
+void exercicio5() {
+    int N;
+    int *V;
+
+    V = criarEPreencherVetorValidado(&N);
+
+    printf("\n--- Vetor final preenchido ---\n");
+    printf("V = [ ");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", V[i]);
+    }
+    printf("]\n");
+
+    free(V);
+}
+
 int main(){
 
 // exercicio 1
-/*
+
     exercicio1();
 
 // exercicio 2
 
     exercicio2();
 
+// exercicio 3
+
+    exercicio3();
+
 // exercicio 4
-*/
+
     exercicio4();
 
+// exercicio 5
+
+    exercicio5();
+    
     return 0; 
 }
