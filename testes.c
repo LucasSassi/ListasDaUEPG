54cdefd830f80941eb37ab01f811726da6989f32
Lucas Sassi de Souza <sassi.lucas17@gmail.com>
2025-10-20 19:52:12 -0300

    estudando arquivo.txt

--- testes.c
+++ testes.c
@@ -2,13 +2,29 @@
 #include <stdio.h>
 
 int main(){
-    FILE *fp;
-    fp = fopen("exemplo1.bin", "wb");
+    // FILE *fp;
+    // fp = fopen("exemplo1.bin", "wb");
 
-    if (fp == NULL) { printf("Erro ao abrir o arquivo.\n"); }
+    // if (fp == NULL) { printf("Erro ao abrir o arquivo.\n"); }
 
-        fclose(fp);
-        // system("pause");
+    //     fclose(fp);
 
-        return 0;
+    //     return 0;
+
+
+    FILE *arq;
+    char string[100];
+    int i;
+    arq = fopen("exemplo2.txt", "w");
+    if (arq == NULL) { printf("Erro ao abrir o arquivo.\n");
+        system("pause");
+        exit(1);
+    }
+    printf("Digite uma string: ");
+    fgets(string, sizeof(string), stdin);
+    fprintf(arq, "%s", string);
+    fclose(arq);
+
+    system("pause");
+
     }
